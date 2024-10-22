4fc1f0e14aba7e5aa79792f5056df93fb0b2199d
shawon325 <mdshahidul3244@gmail.com>
2024-10-23 00:59:53 +0600

    feature: laravel with mysql replication

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/get-employee', function () {
+    return \Illuminate\Support\Facades\DB::table('employees')->get();
+});
+
+Route::get('/store-employee', function () {
+    \Illuminate\Support\Facades\DB::table('employees')->insert([
+        'name' => 'Ridoy',
+        'age' => 25,
+        'salary' => 5000,
+    ]);
+});
